60fdbe67f01a560288b9320f35e0dc93ab883d58
LMJ-01 <144085164+LMJ-01@users.noreply.github.com>
2023-09-12 11:17:11 +0900

    Add files via upload

---
+++ Chapter12-/src/a_Package/test1.java
@@ -0,0 +1,7 @@
+package a_Package;
+
+public class test1 {
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
---
+++ Chapter12-/src/b_Package/test2.java
@@ -0,0 +1,11 @@
+package b_Package;
+import a_Package.test1;
+// import a_Package.*;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		test1 t1 = new test1();
+		System.out.println(t1.sum(10, 20));
+	}
+}
